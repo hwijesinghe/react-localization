8346bce7091eeaaa0db45ac2566eb8e625d6ae63
Hemantha <hwijesinghe@gmail.com>
2021-09-29 21:53:52 +0530

    Localization files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'flag-icon-css/css/flag-icon.min.css'
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+i18n
+  
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    
+    supportedLngs : ['en', 'fr', 'ar'],
+    lng: document.querySelector('html').lang,
+    fallbackLng: "en",
+    detection: {
+      order: ['path', 'cookie', 'htmlTag', 'localStorage',  'subdomain'],
+      caches: ['cookie']
+    },
+    backend:{
+      loadPath: '/locales/{{lng}}/translation.json',
+    }
+    
+    // ,
+    // react:{ useSuspense: false },
+    // interpolation: {
+    //   escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    // }
+
+  });
+
+  const loadingData = (
+    <div className="py-4 text-center" >
+      <h2>Loading...</h2>
+    </div>
+  )
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Suspense fallback={loadingData}>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Suspense>,
+  
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
